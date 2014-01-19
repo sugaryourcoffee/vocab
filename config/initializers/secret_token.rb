7601ca24f76aacdcfa1ecb9a507740d0607e6606
Pierre <pierre@sugaryourcoffee.de>
2014-01-19 20:36:28 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vocab::Application.config.secret_key_base = 'dc39622e4b7f569d9fa074c4e03045a05d2d54fc061f454cbae9c84ec9fc798c4dd8cc0658d520e5e75e5f7a186923a6a2130b5cdf631b26b901712fa58cfa28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vocab::Application.config.session_store :cookie_store, key: '_vocab_session'
