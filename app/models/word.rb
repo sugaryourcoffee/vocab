c99df531b5385780c3f1d547ffff5ea86638e30c
Pierre <pierre@sugaryourcoffee.de>
2014-01-24 06:14:29 +0100

    validate that word in unique

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,6 @@
 class Word < ActiveRecord::Base
   has_many :word_translations
   has_many :translations, through: :word_translations, source: :translation
+
+  validates_uniqueness_of :name, scope: :language
 end
